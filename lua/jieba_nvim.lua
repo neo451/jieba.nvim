7aaf25a92cef12f3899fe1b6c6368f8967a76a81
n451 <2020200706@ruc.edu.cn>
2023-11-11 17:43:47 +0800

    fix hmm parse

--- lua/jieba_nvim.lua
+++ lua/jieba_nvim.lua
@@ -387,7 +387,7 @@ local function navigate(primary_index_func, secondary_index_func, backward, buff
 	-- -- unwrap the row and col from the cursor position
 	local row, col = cursor_pos[1], cursor_pos[2]
 	if row == sentinel_row then
-		pt = parse_tokens(jieba.lcut(buffer[row]))
+		pt = parse_tokens(jieba.lcut(buffer[row], false, true))
 		pt = stack_merge(pt, insert_implicit_space_rule)
 		col = primary_index_func(pt, col)
 
@@ -410,7 +410,7 @@ local function navigate(primary_index_func, secondary_index_func, backward, buff
 		return { row, col }
 	end
 	-- similar steps for when row is not the sentinel_row
-	pt = parse_tokens(jieba.lcut(buffer[row]))
+	pt = parse_tokens(jieba.lcut(buffer[row], false, true))
 	pt = stack_merge(pt, insert_implicit_space_rule)
 	col = primary_index_func(pt, col)
 	if col ~= nil then
@@ -418,7 +418,7 @@ local function navigate(primary_index_func, secondary_index_func, backward, buff
 	end
 	row = row + row_step
 	while row ~= sentinel_row do
-		pt = parse_tokens(jieba.lcut(buffer[row]))
+		pt = parse_tokens(jieba.lcut(buffer[row], false, true))
 		pt = stack_merge(pt, insert_implicit_space_rule)
 		col = secondary_index_func(pt)
 		if col ~= nil then
@@ -426,7 +426,7 @@ local function navigate(primary_index_func, secondary_index_func, backward, buff
 		end
 		row = row + row_step
 	end
-	pt = parse_tokens(jieba.lcut(buffer[row]))
+	pt = parse_tokens(jieba.lcut(buffer[row], false, true))
 	pt = stack_merge(pt, insert_implicit_space_rule)
 	col = secondary_index_func(pt)
 	if col == nil then
