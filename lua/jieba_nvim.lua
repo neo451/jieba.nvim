fce79b9d094be3b4d0706dff22e4ac5360acb0eb
n451 <2020200706@ruc.edu.cn>
2024-12-04 18:34:01 +0800

    fix: add a setup function, option to not use default

--- lua/jieba_nvim.lua
+++ lua/jieba_nvim.lua
@@ -564,4 +564,59 @@ end
 --
 -- vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, { callback = hightlight_under_curosr })
 
+M.setup = function(config)
+	config = config or {}
+	local map = vim.F.if_nil(config.use_default_mappings, true)
+	if map then
+		vim.keymap.set(
+			"n",
+			"B",
+			'<cmd>lua require("jieba_nvim").wordmotion_B()<CR>',
+			{ noremap = false, silent = true }
+		)
+		vim.keymap.set(
+			"n",
+			"b",
+			'<cmd>lua require("jieba_nvim").wordmotion_b()<CR>',
+			{ noremap = false, silent = true }
+		)
+		vim.keymap.set(
+			"n",
+			"w",
+			'<cmd>lua require("jieba_nvim").wordmotion_w()<CR>',
+			{ noremap = false, silent = true }
+		)
+		vim.keymap.set(
+			"n",
+			"W",
+			'<cmd>lua require("jieba_nvim").wordmotion_W()<CR>',
+			{ noremap = false, silent = true }
+		)
+		vim.keymap.set(
+			"n",
+			"E",
+			'<cmd>lua require("jieba_nvim").wordmotion_E()<CR>',
+			{ noremap = false, silent = true }
+		)
+		vim.keymap.set(
+			"n",
+			"e",
+			'<cmd>lua require("jieba_nvim").wordmotion_e()<CR>',
+			{ noremap = false, silent = true }
+		)
+		vim.keymap.set(
+			"n",
+			"ge",
+			'<cmd>lua require("jieba_nvim").wordmotion_ge()<CR>',
+			{ noremap = false, silent = true }
+		)
+		vim.keymap.set(
+			"n",
+			"gE",
+			'<cmd>lua require("jieba_nvim").wordmotion_gE()<CR>',
+			{ noremap = false, silent = true }
+		)
+	end
+end
+
 return M
