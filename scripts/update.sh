bab6eab95d8b71eae2b1240cee54afbb2eac4e9b
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-08-13 00:04:48 +0800

    :hammer: Update shell.nix

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,4 +2,4 @@
 set -e
 cd "$(dirname "$(dirname "$(readlink -f "$0")")")"
 
-nix-shell --run 'luarocks --lua-version=5.1 install --force --local jieba.nvim'
+nix-shell --run 'luarocks install --force jieba.nvim'
--- shell.nix
+++ shell.nix
@@ -6,7 +6,6 @@ with pkgs;
 mkShell {
   name = "jieba.nvim";
   buildInputs = [
-    stdenv.cc
     pkg-config
     xmake
     cmake
@@ -19,8 +18,4 @@ mkShell {
       ]
     ))
   ];
-  # https://github.com/NixOS/nixpkgs/issues/314313#issuecomment-2134252094
-  shellHook = ''
-    LD="$CC"
-  '';
 }
